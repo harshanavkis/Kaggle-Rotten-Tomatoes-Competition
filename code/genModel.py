49758ed196f49447d6d01fe6c0db08597cc7d38f
harshanavkis <harshanavkis@gmail.com>
2017-08-01 14:52:59 +0530

    Code to generate word2vec model of our training data

---
+++ code/genModel.py
@@ -0,0 +1,13 @@
+#using python implementation of word2vec 
+from gensim.models import word2vec
+import logging
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s',level=logging.INFO)
+
+def genModel(sentences,workers=4,size=300,min_count=15,window=10,sample=1e-3):
+	print("Training model...")
+	model=word2vec.Word2Vec(sentences,workers=workers,size=size,min_count=min_count,window=window,sample=sample)
+
+	#make model memory efficient
+	model.init_sims(replace=True)
+	model_name="..//model//300features_15minwords_10context.w2v"
+	model.save(model_name)
